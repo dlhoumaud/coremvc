3085a43f29d63d4d5cb8b18624b83937f4d3a497
David Lhoumaud <david.lhoumaud@afpa.fr>
2024-11-30 14:37:20 +0100

    Revert "Améliorationde l'intégration de vue.js"

--- public/js/components/carousel/item.js
+++ public/js/components/carousel/item.js
@@ -1,12 +1,10 @@
-export default (app) => {
-    app.component("carousel-item", {
-        props: ["imgsrc", "imgalt", "title", "text", "interval", "active"],
-        template: `<div class="carousel-item" :class="{ active: active }" :data-bs-interval="interval">
-        <img :src="imgsrc" class="d-block w-100" :alt="imgalt">
-        <figcaption class="carousel-caption d-none d-md-block">
-            <h5>{{ title }}</h5>
-            <p><slot name="text">{{ text }}</slot></p>
-        </figcaption>
-    </div>`,
-    });
-}
+app.component("carousel-item", {
+    props: ["imgsrc", "imgalt", "title", "text", "interval", "active"],
+    template: `<div class="carousel-item" :class="{ active: active }" :data-bs-interval="interval">
+    <img :src="imgsrc" class="d-block w-100" :alt="imgalt">
+    <figcaption class="carousel-caption d-none d-md-block">
+        <h5>{{ title }}</h5>
+        <p><slot name="text">{{ text }}</slot></p>
+    </figcaption>
+</div>`,
+});
